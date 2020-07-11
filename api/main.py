b3d06cf4f3d3f2a366bed208d449e1a34df9b305
Jose Manuel Abreu Medina <jabreu@tigo.com.gt>
2020-07-10 18:41:49 -0600

    Initial Sentiment Analysis Version Commit

---
+++ api/main.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from fastapi import FastAPI, Query
+
+from src.models.train import train
+from src.models.predict import predict
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+app = FastAPI()
+
+
+@app.post('/')
+async def train_model():
+    train()
+
+    return {'Result': 'model.pkl produced'}
+
+
+@app.get('/predict')
+async def predict_review(sentences: List[str] = Query(..., description='Sentences to process')):
+    predictions = predict(sentences)
+
+    response = [
+        {
+            'id': idx + 1,
+            'sentence': sentence,
+            'prediction': sentiment
+        }
+        for idx, (sentence, sentiment) in enumerate(zip(sentences, predictions))
+    ]
+
+    return response
